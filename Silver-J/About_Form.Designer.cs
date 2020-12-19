76ea9d46ffe3761f279d1679f43d5beef50610fd
Soubik <bhui_soubik@outlook.com>
2020-12-19 08:46:48 +0530

    Initial Commit

--- Silver-J/About_Form.Designer.cs
+++ Silver-J/About_Form.Designer.cs
@@ -58,8 +58,6 @@ namespace Silver_J
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(About_Form));
             this.panelZ1 = new Silver_J.PanelZ();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label5 = new System.Windows.Forms.Label();
@@ -74,8 +72,6 @@ namespace Silver_J
             // 
             // panelZ1
             // 
-            this.panelZ1.Controls.Add(this.label7);
-            this.panelZ1.Controls.Add(this.label8);
             this.panelZ1.Controls.Add(this.label6);
             this.panelZ1.Controls.Add(this.pictureBox1);
             this.panelZ1.Controls.Add(this.label5);
@@ -94,30 +90,6 @@ namespace Silver_J
             this.panelZ1.TabIndex = 0;
             this.panelZ1.Transparent1 = 255;
             this.panelZ1.Transparent2 = 255;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.BackColor = System.Drawing.Color.Transparent;
-            this.label7.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.ForeColor = System.Drawing.Color.White;
-            this.label7.Location = new System.Drawing.Point(171, 172);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(80, 28);
-            this.label7.TabIndex = 13;
-            this.label7.Text = "Soubik";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.BackColor = System.Drawing.Color.Transparent;
-            this.label8.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.ForeColor = System.Drawing.Color.White;
-            this.label8.Location = new System.Drawing.Point(67, 177);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(98, 19);
-            this.label8.TabIndex = 12;
-            this.label8.Text = "Redesigned By";
             // 
             // label6
             // 
@@ -247,8 +219,6 @@ namespace Silver_J
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.PictureBox pictureBox1;
     }
